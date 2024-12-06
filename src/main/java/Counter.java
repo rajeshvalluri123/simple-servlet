70e8dda9e45118afbda9de3afe3ba3ea86f425ea
rajeshvalluri123 <70281671+rajeshvalluri123@users.noreply.github.com>
2024-12-06 19:53:47 +0530

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,33 +1,18 @@
-// import java.io.*;
-// import javax.servlet.*;
-// import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-// public class Counter extends HttpServlet {
-//     int accesses = 0;
+public class Counter extends HttpServlet {
+    int accesses = 0;
 
-//     public void doGet(HttpServletRequest request, HttpServletResponse response)
-//          throws IOException, ServletException
-//     {
-//         response.setContentType("text/html");
-//         PrintWriter out = response.getWriter();
-
-//         accesses++;
-//         out.print("Number of times this servlet has been accessed:" + accesses);
-//     }
-// } 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
- 
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException
+    {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        PrintWriter out = response.getWriter();
+
+        accesses++;
+        out.print("Number of times this servlet has been accessed:" + accesses);
     }
-}
+} 
+
